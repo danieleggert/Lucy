d029ed99fe3081d07d122c97cdff24ed11b1dd70
Daniel Eggert <daniel.eggert@wearezeta.com>
2013-09-19 12:39:11 +0200

    Add sample iOS project using the parser.

---
+++ AutoLayoutApp/AutoLayoutApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AutoLayoutApp
+//
+//  Created by Daniel Eggert on 19/09/13.
+//  Copyright (c) 2013 objc.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ AutoLayoutApp/AutoLayoutAppTests/AutoLayoutAppTests.m
@@ -0,0 +1,34 @@
+//
+//  AutoLayoutAppTests.m
+//  AutoLayoutAppTests
+//
+//  Created by Daniel Eggert on 19/09/13.
+//  Copyright (c) 2013 objc.io. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoLayoutAppTests : XCTestCase
+
+@end
+
+@implementation AutoLayoutAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

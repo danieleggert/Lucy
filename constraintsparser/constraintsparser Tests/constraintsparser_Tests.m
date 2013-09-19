8127614b42c99ff03bcea5b7cd93bac296c13293
Daniel Eggert <daniel.eggert@wearezeta.com>
2013-09-19 10:17:44 +0200

    Initial commit.

---
+++ constraintsparser/constraintsparser Tests/constraintsparser_Tests.m
@@ -0,0 +1,34 @@
+//
+//  constraintsparser_Tests.m
+//  constraintsparser Tests
+//
+//  Created by Daniel Eggert on 18/09/13.
+//  Copyright (c) 2013 objc.io. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface constraintsparser_Tests : XCTestCase
+
+@end
+
+@implementation constraintsparser_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
---
+++ constraintsparser/constraintsparser/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  constraintsparser
+//
+//  Created by Daniel Eggert on 18/09/13.
+//  Copyright (c) 2013 objc.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
